c558d1851b619b4e646a600675aa257cf5b08e22
rohitaryal <erucix@gmail.com>
2024-06-12 13:24:11 +0530

    Added sherlock.py

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# sherlock
-Find your KIIT buddy's details using his/her roll number only.
+## 🧞 Sherlock
+Find your KIIT buddy's details using his/her roll number only 🧠.
+Details include 
+- Name
+- E-Mail
+- Phone Number
+- Branch/Program
+
+## Requirements
+- Python3.x
+- `requests` library
+
+> `pip install requests`
+
+## 👓 Usage
+Open up your terminal window and paste this
+```bash
+git clone https://github.com/rohitaryal/Sherlock
+cd Sherlock
+python3 sherlock.py # <-- Now continue yourself
+```
+
+🎃 **Or you can import this script.**
+```python
+from sherlock import Sherlock
+
+detective=Sherlock()
+print(detective.find("ROLL_NUMBER"))
+```
+🙀 **Outputs:**
+```JSON
+{
+    'status': 'success',
+    'student_id': '1234567890', 
+    'phone_no': '1234567890', 
+    'email_id': 'my_mail@gmail.com', 
+    'roll_no': 'ROLL_NUMBER', 
+    'name': 'ROHIT  SHARMA', 
+    'is_international': True
+    'program_des': 'BRANCH DESCRIPTION,  YEAR'
+ }
+```
+---
+### 🤔 TODO
+
+- Put comments 💀
+
+### 🤮 Issues?
+Leave an issue with error logs 
---
+++ sherlock.py
@@ -0,0 +1,93 @@
+import requests as fetch
+import re
+import urllib.parse as url
+
+text = {
+    "q": "\033[34m[ \033[1;35m?\033[34m ]\033[0m",
+    "w": "\033[34m[ \033[31m!\033[34m ]\033[0m",
+    "s": "\033[32m[ \033[1;32m+\033[32m ]\033[0m",
+    "l": "\033[33m[ \033[1;33m*\033[33m ]\033[0m",
+    "c": "\033[1;36m:\033[0m"
+}
+
+class Sherlock:
+    def __init__(self):
+        self.payment_post_url = "https://form.kiit.ac.in/payment/";
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:122.0) Gecko/20100101 Firefox/122.0",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Origin": "https://form.kiit.ac.in"
+        }
+
+    
+    def find(self, roll_no):
+        first_request = fetch.post(self.payment_post_url, headers=self.headers, data={"appno": roll_no}, allow_redirects=False)
+        first_request.close();
+        
+        # Notice that we have allow_redirect=False inorder to idenify the status_code
+        if first_request.status_code != 302:
+            return {
+                "status":"error",
+                "message": "Roll number doesn't exist"
+            }
+            
+        print(f"{text['s']} Roll Number found")
+            
+        if not "location" in first_request.headers:
+            return {
+                "status":"error",
+                "message": "No redirection URL found"
+            }
+        
+        if __name__ == "__main__":
+            print(f"{text['l']} Parsing HTML")
+        
+        redirect_url = first_request.headers["location"]
+        submit_payment_url = url.urljoin(self.payment_post_url, redirect_url)
+        second_request = fetch.post(submit_payment_url, headers=self.headers, data={'amount': '100000000'})
+        
+        if second_request.status_code != 200:
+            return {
+                "status":"error",
+                "message": f"Server responded with status code {second_request.status_code}"
+            }
+            
+        student_id=re.search(r"name=\"student_id\"\svalue=\"(.*)\"", second_request.text).group(1)
+        phone_no=re.search(r"name=\"phone\"\svalue=\"(.*)\"", second_request.text).group(1)
+        email_id=re.search(r"name=\"email\"\svalue=\"(.*)\"", second_request.text).group(1)
+        name=re.search(r"name=\"name\"\svalue=\"(.*)\"", second_request.text).group(1)
+        program_des=re.search(r"name=\"Program_Description\"\svalue=\"(.*)\"", second_request.text).group(1)
+        
+        return {
+            "status": "success",
+            "student_id": student_id,
+            "phone_no": "[REDACTED]" if phone_no == "0000000000" else phone_no,
+            "email_id": email_id,
+            "roll_no": roll_no,
+            "name": name,
+            "program_des": program_des,
+            "is_international": True if phone_no == "0000000000" else False
+        }
+
+if __name__ == "__main__":
+    spongebob=Sherlock()
+    while True:
+        roll=input(f"{text['q']} Roll Number: ")
+        print(f"{text['l']} Searching.")
+        
+        details=spongebob.find(roll)
+        
+        if details["status"] != "error":
+            print(f"{text['s']} Name          {text['c']} " + details["name"])
+            print(f"{text['s']} E-Mail        {text['c']} " + details["email_id"])
+            print(f"{text['s']} Branch        {text['c']} " + details["program_des"])
+            print(f"{text['s']} Status        {text['c']} " + details["status"])
+            print(f"{text['s']} Phone No      {text['c']} " + details["phone_no"])
+            print(f"{text['s']} International {text['c']}", details["is_international"])
+            print(f"{text['s']} Roll Number   {text['c']} " + details["roll_no"])
+            print(f"{text['s']} Student ID    {text['c']} " + details["student_id"])
+        else:
+            print(text["w"] + details["message"])
